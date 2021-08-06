0206b9f152fc91f115595daaeaeb8694e2f8b2c8
Esko Luontola <esko@luontola.fi>
2021-08-06 20:31:25 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject new-clojure-project "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [medley "1.3.0"]]
-  :managed-dependencies [[org.clojure/spec.alpha "0.2.187"]]
+  :managed-dependencies [[org.clojure/spec.alpha "0.2.194"]]
   :pedantic? :abort
   :global-vars {*warn-on-reflection* true}
 
@@ -15,6 +15,6 @@
   :plugins [[lein-ancient "0.6.15"]]
 
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[lambdaisland/kaocha "1.0.700"]
+             :dev {:dependencies [[lambdaisland/kaocha "1.0.861"]
                                   [org.clojure/test.check "1.1.0"]]}
              :kaocha {}})
