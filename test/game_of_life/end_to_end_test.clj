ac0b6e51494ccd39209916f9481358b14a1061f0
Esko Luontola <esko@luontola.fi>
2022-06-03 00:14:57 +0300

    Add an e2e test that has some visible effect: blinker

--- test/game_of_life/end_to_end_test.clj
+++ test/game_of_life/end_to_end_test.clj
@@ -1,6 +1,6 @@
 (ns game-of-life.end-to-end-test
   (:require [clojure.string :as str]
-            [clojure.test :refer [deftest is]]
+            [clojure.test :refer :all]
             [game-of-life.cli :as cli]))
 
 (defn normalize-whitespace [s]
@@ -10,6 +10,12 @@
       (str "\n")))
 
 (deftest end-to-end-test
-  (is (= (normalize-whitespace (slurp "testdata/block.rle"))
-         (with-out-str
-           (cli/-main "testdata/block.rle" "1")))))
+  (testing "block - a still life"
+    (is (= (normalize-whitespace (slurp "testdata/block.rle"))
+           (with-out-str
+             (cli/-main "testdata/block.rle" "1")))))
+
+  (testing "blinker - an oscillator"
+    (is (= (normalize-whitespace (slurp "testdata/blinker-t1.rle"))
+           (with-out-str
+             (cli/-main "testdata/blinker.rle" "3"))))))
