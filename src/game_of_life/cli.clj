5cc7db9a2d0c5f0d3ea1154a5bc1f2884dc287a3
Esko Luontola <esko@luontola.fi>
2022-06-02 18:44:09 +0300

    Parse RLE file into sections

--- src/game_of_life/cli.clj
+++ src/game_of_life/cli.clj
@@ -1,5 +1,21 @@
 (ns game-of-life.cli
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn parse-rle-pattern [data]
+  (reduce (fn [result line]
+            (case (first line)
+              \# (update result :hash-lines conj line)
+              \x (assoc result :header-line line)
+              (update result :encoded-pattern str line)))
+          {:hash-lines []
+           :header-line nil
+           :encoded-pattern ""}
+          (str/split-lines data)))
 
 (defn -main [& [input-file iterations]]
-  (println (slurp input-file)))
+  (let [data (slurp input-file)
+        parsed (parse-rle-pattern data)]
+    (println (str/join "\n" (:hash-lines parsed)))
+    (println (:header-line parsed))
+    (println (:encoded-pattern parsed))))
--- test/game_of_life/end_to_end_test.clj
+++ test/game_of_life/end_to_end_test.clj
@@ -1,8 +1,15 @@
 (ns game-of-life.end-to-end-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is]]
             [game-of-life.cli :as cli]))
 
+(defn normalize-whitespace [s]
+  (-> s
+      (str/replace "\r\n" "\n")
+      (str/trim)
+      (str "\n")))
+
 (deftest end-to-end-test
-  (is (= (str (slurp "testdata/block.rle") "\n")
+  (is (= (normalize-whitespace (slurp "testdata/block.rle"))
          (with-out-str
            (cli/-main "testdata/block.rle" "1")))))
