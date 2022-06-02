8faac5bf3119502868ae96f6106419049a18216e
Esko Luontola <esko@luontola.fi>
2022-06-03 00:19:21 +0300

    Normalize the test data's whitespace
    - Change from CRLF to LF line endings
    - Add LF to end of file

--- test/game_of_life/end_to_end_test.clj
+++ test/game_of_life/end_to_end_test.clj
@@ -1,21 +1,14 @@
 (ns game-of-life.end-to-end-test
-  (:require [clojure.string :as str]
-            [clojure.test :refer :all]
+  (:require [clojure.test :refer :all]
             [game-of-life.cli :as cli]))
-
-(defn normalize-whitespace [s]
-  (-> s
-      (str/replace "\r\n" "\n")
-      (str/trim)
-      (str "\n")))
 
 (deftest end-to-end-test
   (testing "block - a still life"
-    (is (= (normalize-whitespace (slurp "testdata/block.rle"))
+    (is (= (slurp "testdata/block.rle")
            (with-out-str
              (cli/-main "testdata/block.rle" "1")))))
 
   (testing "blinker - an oscillator"
-    (is (= (normalize-whitespace (slurp "testdata/blinker-t1.rle"))
+    (is (= (slurp "testdata/blinker-t1.rle")
            (with-out-str
              (cli/-main "testdata/blinker.rle" "3"))))))
